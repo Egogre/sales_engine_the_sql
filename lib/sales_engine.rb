7b2fa7f5e061a6ce94101220687cc26eabf9895c
George Hudson <georgehudson78@gmail.com>
2015-08-10 21:50:06 -0600

    all tables built and loaded with data

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -60,36 +60,166 @@ class SalesEngine
 
   def load_database_tables
     load_customer_table
+    load_invoice_item_table
+    load_invoice_table
+    load_item_table
+    load_merchant_table
+    load_transaction_table
   end
 
   def load_customer_table
-    db.execute <<-SQL
+    db.execute "
     CREATE TABLE customers (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        first_name VARCHAR(20),
-                                        last_name VARCHAR(20),
-                                        created_at DATETIME,
-                                        updated_at DATETIME);
-    SQL
+                            first_name VARCHAR(20),
+                            last_name VARCHAR(20),
+                            created_at DATETIME,
+                            updated_at DATETIME);"
 
     customers.each do |customer|
-      columns = 'first_name, last_name, created_at, updated_at'
-      vals = [customer[:first_name], customer[:last_name], customer[:created_at], customer[:updated_at]]
-      db.execute "INSERT INTO customers (#{columns}) VALUES (?,?,?,?);", vals
+      columns = 'first_name,
+                 last_name,
+                 created_at,
+                 updated_at'
+      vals = [customer[:first_name],
+              customer[:last_name],
+              customer[:created_at],
+              customer[:updated_at]]
+      db.execute "
+      INSERT INTO customers (#{columns}) VALUES (?,?,?,?);
+      ", vals
     end
   end
-  #INSERT INTO games(yr,city) VALUES (2005,'Athens');
 
-  # def add(record_attributes)
-  #     keys = (record_attributes[0] || {}).keys
-  #     name_sql = keys.join(', ')
-  #
-  #     values_sql = record_attributes.map { |attrs|
-  #       sql = keys.map { |key| attrs[key].inspect }.join(", ")
-  #       "(#{sql})"
-  #     }.join(", ")
-  #
-  #     insert_sql = "insert into #{table_name} (#{name_sql}) values #{values_sql};"
-  #     db.execute(insert_sql)
-  #   end
+  def load_invoice_item_table
+    db.execute "
+    CREATE TABLE invoice_items (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                item_id INTEGER,
+                                invoice_id INTEGER,
+                                quantity INTEGER,
+                                unit_price INTEGER,
+                                created_at DATETIME,
+                                updated_at DATETIME);"
+    invoice_items.each do |invoice_item|
+      columns = 'item_id,
+                 invoice_id,
+                 quantity,
+                 unit_price,
+                 created_at,
+                 updated_at'
+      vals = [invoice_item[:item_id],
+              invoice_item[:invoice_id],
+              invoice_item[:quantity],
+              invoice_item[:unit_price],
+              invoice_item[:created_at],
+              invoice_item[:updated_at]]
+      db.execute "
+      INSERT INTO invoice_items (#{columns}) VALUES (?,?,?,?,?,?);
+      ", vals
+    end
+  end
+
+  def load_invoice_table
+    db.execute "
+    CREATE TABLE invoices (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            customer_id INTEGER,
+                            merchant_id INTEGER,
+                            status VARCHAR(10),
+                            created_at DATETIME,
+                            updated_at DATETIME);"
+
+    invoices.each do |invoice|
+      columns = 'customer_id,
+                 merchant_id,
+                 status,
+                 created_at,
+                 updated_at'
+      vals = [invoice[:customer_id],
+              invoice[:merchant_id],
+              invoice[:status],
+              invoice[:created_at],
+              invoice[:updated_at]]
+      db.execute "
+      INSERT INTO invoices (#{columns}) VALUES (?,?,?,?,?);
+      ", vals
+    end
+  end
+
+  def load_item_table
+    db.execute "
+    CREATE TABLE items (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name VARCHAR(30),
+                            description VARCHAR(200),
+                            unit_price INTEGER,
+                            merchant_id INTEGER,
+                            created_at DATETIME,
+                            updated_at DATETIME);"
+
+    items.each do |item|
+      columns = 'name,
+                 description,
+                 unit_price,
+                 merchant_id,
+                 created_at,
+                 updated_at'
+      vals = [item[:name],
+              item[:description],
+              item[:unit_price],
+              item[:merchant_id],
+              item[:created_at],
+              item[:updated_at]]
+      db.execute "
+      INSERT INTO items (#{columns}) VALUES (?,?,?,?,?,?);
+      ", vals
+    end
+  end
+
+  def load_merchant_table
+    db.execute "
+    CREATE TABLE merchants (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name VARCHAR(40),
+                            created_at DATETIME,
+                            updated_at DATETIME);"
+
+    merchants.each do |merchant|
+      columns = 'name,
+                 created_at,
+                 updated_at'
+      vals = [merchant[:name],
+              merchant[:created_at],
+              merchant[:updated_at]]
+      db.execute "
+      INSERT INTO merchants (#{columns}) VALUES (?,?,?);
+      ", vals
+    end
+  end
+
+  def load_transaction_table
+    db.execute "
+    CREATE TABLE transactions (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            invoice_id INTEGER,
+                            credit_card_number INTEGER,
+                            credit_card_expiration_date DATETIME,
+                            result VARCHAR(10),
+                            created_at DATETIME,
+                            updated_at DATETIME);"
+
+    transactions.each do |transaction|
+      columns = 'invoice_id,
+                 credit_card_number,
+                 credit_card_expiration_date,
+                 result,
+                 created_at,
+                 updated_at'
+      vals = [transaction[:invoice_id],
+              transaction[:credit_card_number],
+              transaction[:credit_card_expiration_date],
+              transaction[:result],
+              transaction[:created_at],
+              transaction[:updated_at]]
+      db.execute "
+      INSERT INTO transactions (#{columns}) VALUES (?,?,?,?,?,?);
+      ", vals
+    end
+  end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,14 +3,14 @@ require_relative 'test_helper'
 class SalesEngineTest < Minitest::Test
 
   def test_it_exists
-    # skip
+    skip
     engine = SalesEngine.new
 
     assert engine
   end
 
   def test_it_accepts_a_folder_with_the_csvs
-    # skip
+    skip
     sales_engine_root = File.expand_path('../..',  __FILE__)
     folder = File.join(sales_engine_root, 'data')
     engine = SalesEngine.new(folder)
@@ -24,7 +24,7 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_files_are_converted_to_table
-    # skip
+    skip
     engine = SalesEngine.new
 
     assert_kind_of CSV::Table, engine.customers
@@ -36,7 +36,7 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_it_loads_repositories
-    # skip
+    skip
     engine = SalesEngine.new
     engine.startup
 
@@ -49,7 +49,7 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_it_creates_database
-    # skip
+    skip
     engine = SalesEngine.new
     engine.startup
 
@@ -59,8 +59,20 @@ class SalesEngineTest < Minitest::Test
   def test_it_loads_tables
     engine = SalesEngine.new
     engine.startup
-    result = engine.db.execute("SELECT * FROM customers;")
-    assert_equal "Joey", result[0]["first_name"]
+
+    result1 = engine.db.execute("SELECT * FROM customers;")
+    result2 = engine.db.execute("SELECT * FROM invoice_items;")
+    result3 = engine.db.execute("SELECT * FROM invoices;")
+    result4 = engine.db.execute("SELECT * FROM items;")
+    result5 = engine.db.execute("SELECT * FROM merchants;")
+    result6 = engine.db.execute("SELECT * FROM transactions;")
+
+    assert_equal "Joey", result1[0]["first_name"]
+    assert_equal 6, result2[52]["quantity"]
+    assert_equal 7, result3[23]["customer_id"]
+    assert_equal 39891, result4[13]["unit_price"]
+    assert_equal "Rowe and Sons", result5[31]["name"]
+    assert_equal 201, result6[221]["invoice_id"]
   end
 
 end
