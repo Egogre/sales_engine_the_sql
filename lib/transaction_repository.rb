c0e2c893059ddcfca7ee1ac56495bb2a4b982bd4
George Hudson <georgehudson78@gmail.com>
2015-08-11 18:03:26 -0600

    transaction repo converted for sql

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -7,4 +7,16 @@ class TransactionRepository
     'transactions'
   end
 
+  def find_by_credit_card_number(card_number)
+    db.execute("
+    SELECT * FROM transactions WHERE credit_card_number = #{card_number};
+    ")
+  end
+
+  def find_all_by_result(query_result)
+    db.execute("
+    SELECT * FROM transactions WHERE result = '#{query_result}';
+    ")
+  end
+
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -3,42 +3,18 @@ require_relative 'test_helper'
 class TransactionRepositoryTest < Minitest::Test
 attr_reader :transaction_repo
 
-  def setup
-    csv_table = [{
-                  :id => 1,
-                  :invoice_id => 4,
-                  :credit_card_number => 1234123412341234,
-                  :credit_card_expiration_date => "09-12-15",
-                  :result => "success"
-                 },
-                 {
-                  :id => 2,
-                  :invoice_id => 4,
-                  :credit_card_number => 1111222233334444,
-                  :credit_card_expiration_date => "04-12-12",
-                  :result => "success"
-                 },
-                 {
-                  :id => 3,
-                  :invoice_id => 4,
-                  :credit_card_number => 4321432143214321,
-                  :credit_card_expiration_date => "09-08-17",
-                  :result => "failed"
-                 }]
-    engine = "Pretend Engine"
-    @transaction_repo = TransactionRepository.new(csv_table, engine)
-  end
+def setup
+  engine = SalesEngine.new
+  engine.startup
+  @transaction_repo = engine.transaction_repository
+end
 
   def test_it_exists
     assert transaction_repo
   end
 
-  def test_it_converts_to_hash
-    assert_kind_of Hash, transaction_repo.records
-  end
-
   def test_all_returns_all_transactions
-    assert_equal 3, transaction_repo.all.length
+    assert_equal 5595, transaction_repo.all.length
   end
 
   def test_random
@@ -48,6 +24,26 @@ attr_reader :transaction_repo
     end
 
     refute_equal 1, instances.uniq.length
+  end
+
+  def test_find_by_id
+    transaction = transaction_repo.find_by_id(432)
+
+    assert_equal 372, transaction[0]["invoice_id"]
+  end
+
+  def test_find_all_by_invoice_id
+    assert_equal 74, transaction_repo.find_all_by_invoice_id(66)[0]["id"]
+  end
+
+  def test_find_by_credit_card_number
+    card_number = 4017503416578382
+    transaction = transaction_repo.find_by_credit_card_number(card_number)
+    assert_equal "failed", transaction[0]["result"]
+  end
+
+  def test_find_all_by_result
+    assert_equal 947, transaction_repo.find_all_by_result("failed").count
   end
 
 end
